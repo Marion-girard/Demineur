038efb2a6f9d6013413cb228961911dc8b2fbc29
Leo <leo.clouet@3wa.io>
2023-09-20 13:22:40 +0200

    géneration des pièges

--- plateau.js
+++ plateau.js
@@ -31,7 +31,27 @@ let plateau = document.querySelector('#plateau');
 for (let j = 0; j < 10; j++) {
 
   for (let i = 0; i < 10; i++) {
+    const caseId = i + "-" + j;
+
     plateau.innerHTML += "<div id='" + i + "-" + j + "' class='block'></div>";
   }
   plateau.innerHTML += "<br>"
 }
+
+function generateTraps(nbrTraps, maxX, maxY) {
+  const traps = [];
+
+  for (let i = 0; i < nbrTraps; i++) {
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+    traps.push({x, y});
+  }
+  return traps;
+}
+
+const nombreDePieges = 10;
+const maxX = 10;
+const maxY = 10;
+const piegesGeneres = generateTraps(nombreDePieges, maxX, maxY);
+console.log("Coordonées des pièges :");
+console.log(piegesGeneres);
